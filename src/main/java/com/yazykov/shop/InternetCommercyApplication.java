4d123b5619390bfc9fdb1f79069f97a511178b3a
hide-tech <hidetechyy@gmail.com>
2022-07-04 11:59:25 +0300

    Initial commit

---
+++ src/main/java/com/yazykov/shop/InternetCommercyApplication.java
@@ -0,0 +1,13 @@
+package com.yazykov.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetCommercyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetCommercyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yazykov/shop/InternetCommercyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazykov.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetCommercyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
